b1e3605abbaf8b6b449a80dbe415505a461cbe0e
erfdev <erfsnet@gmail.com>
2018-08-05 18:23:15 -0400

    Added detect apple script, added initial trained xml file, still needs more training for better accuracy.

---
+++ src/obj_detection/detect_apple.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+apple_cascade = cv.CascadeClassifier('cascade.xml')
+img = cv.imread('ss1.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+apples = apple_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in apples:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+cv.imshow('img',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
