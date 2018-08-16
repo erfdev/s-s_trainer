2799e32f04619bcbfaa850730e4059f2959ecc27
erfdev <erfsnet@gmail.com>
2018-08-15 21:44:56 -0400

    Submitting updated detect script and cascade xml file missed from last submission.

--- src/obj_detection/detect_apple.py
+++ src/obj_detection/detect_apple.py
@@ -1,16 +1,31 @@
 import numpy as np
 import cv2 as cv
 apple_cascade = cv.CascadeClassifier('cascade.xml')
-img = cv.imread('ss1.png')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#img = cv.imread('ss3.png')
 
-apples = apple_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in apples:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-cv.imshow('img',img)
-cv.waitKey(0)
+cap = cv.VideoCapture('g1.mp4')
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    apples = apple_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in apples:
+        if w > 90 : 
+            continue
+        if h > 90 : 
+            continue
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    cv.imshow('img',img)
+    cv.waitKey(1) #FIXME 
+
+
+
+while cv.waitKey() != 27 :
+    i = 1	
+
+cap.release()
 cv.destroyAllWindows()
 
 
